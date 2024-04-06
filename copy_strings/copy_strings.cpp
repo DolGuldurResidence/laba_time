3e5196f7c1e5120356f144928703fdc2b6ff6c9b
DolGuldurResidence <157448659+DolGuldurResidence@users.noreply.github.com>
2024-04-06 09:00:17 +0300

    add copy_strings lab

---
+++ copy_strings/copy_strings.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class MyString {
+private:
+    char* str;
+    int length;
+public:
+    MyString(const char* s) {
+        length = strlen(s);
+        str = new char[length + 1];
+        strcpy(str, s);
+    }
+    // Конструктор копирования
+    MyString(const MyString& other) {
+        length = other.length;
+        str = new char[length + 1];
+        strcpy(str, other.str);
+    }
+    // Оператор присваивания копированием
+    MyString& operator=(const MyString& other) {
+        if (this != &other) {
+            delete[] str;
+            length = other.length;
+            str = new char[length + 1];
+            strcpy(str, other.str);
+        }
+        return *this;
+    }
+    ~MyString() {
+        delete[] str;
+    }
+    void print() {
+        std::cout << str << std::endl;
+    }
+};
+int main() {
+    MyString str1("Hello");
+    MyString str2 = str1; // Вызов конструктора копирования
+    MyString str3("World");
+    str2 = str3 = str1; // Вызов оператора присваивания копированием
+    str1.print();
+    str2.print();
+    str3.print();
+
+    return 0;
+}
+
